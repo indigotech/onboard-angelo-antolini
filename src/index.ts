a247c14e3ce0cae5b59e698763be629e8e011ccb
Angeloantolini <angelo.antolini@hotmail.com>
2021-09-09 16:48:13 -0300

    criando mutation

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import { User } from './entity/User';
 
-createConnection()
-  .then(async (connection) => {
-    console.log('Inserting a new user into the database...');
-    const user = new User();
-    user.firstName = 'Angelo';
-    user.lastName = 'Antolini';
-    user.age = 21;
-    await connection.manager.save(user);
-    console.log('Saved a new user with id: ' + user.id);
+createConnection();
+// .then(async (connection) => {
+//   console.log('Inserting a new user into the database...');
+//   const user = new User();
+//   user.firstName = 'Angelo';
+//   user.lastName = 'Antolini';
+//   user.age = 21;
+//   await connection.manager.save(user);
+//   console.log('Saved a new user with id: ' + user.id);
 
-    console.log('Loading users from the database...');
-    const users = await connection.manager.find(User);
-    console.log('Loaded users: ', users);
+//   console.log('Loading users from the database...');
+//   const users = await connection.manager.find(User);
+//   console.log('Loaded users: ', users);
 
-    console.log('Here you can setup and run express/koa/any other framework.');
-  })
-  .catch((error) => console.log(error));
+//   console.log('Here you can setup and run express/koa/any other framework.');
+// })
+// .catch((error) => console.log(error));

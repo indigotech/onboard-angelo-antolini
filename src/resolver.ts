0aab6913cf27970e121cfb18093915449b198d06
Angeloantolini <angelo.antolini@hotmail.com>
2021-09-10 11:32:49 -0300

    teste mutation

--- src/resolver.ts
+++ src/resolver.ts
@@ -2,6 +2,17 @@ import { User } from './entity/User';
 
 export const resolvers = {
   Mutation: {
-    createUser: async () => {},
+    addUser: async (_, { name, email, password, birthDate }) => {
+      const storeUser = {
+        id: 12,
+        name,
+        email,
+        password,
+        birthDate,
+      };
+
+      User.create(storeUser);
+      return true;
+    },
   },
 };

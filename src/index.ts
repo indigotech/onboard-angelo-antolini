49fd84f1486238ca3dad2b76785289aa698964d7
Angeloantolini <angelo.antolini@hotmail.com>
2021-09-13 09:58:11 -0300

    synchronize

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,15 @@ import { ApolloServer } from 'apollo-server';
 
 import { typeDefs } from './schema';
 import { resolvers } from './resolver';
+import { url } from 'inspector';
 
 const startServer = async () => {
   await createConnection().catch((error) => console.log(error));
   console.log('connection with database OK');
 
   const server = new ApolloServer({ typeDefs, resolvers });
-  server
-    .listen()
-    .then(({ url }) => {
-      console.log(`Server running on ${url}`);
-    })
-    .catch((error) => console.log(error));
+  const { url } = await server.listen();
+  console.log(`Server running on: ${url}`);
 };
 
 startServer();

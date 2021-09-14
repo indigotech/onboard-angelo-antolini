94208c5c73c234532618a180425f8b458421e2ba
Angeloantolini <87835346+Angeloantolini@users.noreply.github.com>
2021-09-14 17:30:31 -0300

    Revert "Ajustes do CR"
    This reverts commit 3bd0351b8213cc4729a6957fdbe060091f94c12a.

--- src/resolver.ts
+++ src/resolver.ts
@@ -17,20 +17,21 @@ export const resolvers = {
       user.password = Password;
       user.birthDate = BirthDate;
 
-      let validInput = true;
+      let validPasswordLength = true;
+      let validPasswordLetter = true;
+      let validPasswordNumber = true;
+      let validEmail = true;
+
+      user.password.length < 7 ? (validPasswordLength = false) : validPasswordLength;
+      user.password.search(/[0-9]/) == -1 ? (validPasswordNumber = false) : validPasswordNumber;
+      user.password.search(/[a-z]/) == -1 && user.password.search(/[A-Z]/) == -1
+        ? (validPasswordLetter = false)
+        : validPasswordLetter;
+
       const sameEmail = await repository.find({ email: user.email });
+      sameEmail.length == 0 ? validEmail : (validEmail = false);
 
-      if (user.password.length < 7) {
-        validInput = false;
-      } else if (user.password.search(/[0-9]/) == -1) {
-        validInput = false;
-      } else if (user.password.search(/[a-z]/) == -1 && user.password.search(/[A-Z]/) == -1) {
-        validInput = false;
-      } else if (sameEmail.length !== 0) {
-        validInput = false;
-      }
-
-      if (validInput) {
+      if (validEmail && validPasswordLength && validPasswordLetter && validPasswordNumber) {
         const response = await repository.save(user);
 
         const outputUser = {
@@ -42,7 +43,10 @@ export const resolvers = {
 
         return outputUser;
       } else {
-        console.error('Usuário Inválido');
+        const failUser = {
+          Name: 'Invalid User',
+        };
+        return failUser;
       }
     },
   },

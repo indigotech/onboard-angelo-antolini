30ffa02d92e4b40750fb60a951e5d31d4e2fc40c
Angeloantolini <angelo.antolini@hotmail.com>
2021-09-20 16:42:04 -0300

    teste do login

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,9 +1,5 @@
 import { User } from './entity/User';
 import { getRepository } from 'typeorm';
-<<<<<<< HEAD
-=======
-import { UserInputError } from 'apollo-server';
->>>>>>> 6b1acd3... autenticacao
 import { compare, hash } from 'bcrypt';
 import { CustomError } from './errors';
 import { LonginInput, UserInput } from './schema-types';
@@ -32,7 +28,6 @@ export const resolvers = {
     },
   },
   Mutation: {
-<<<<<<< HEAD
     createUser: async (_, { data: args }: { data: UserInput }, context) => {
       if (!context.token) {
         throw new CustomError('Voce nao tem permissao para efetuar essa açao', 404, 'token not found');
@@ -41,54 +36,6 @@ export const resolvers = {
       const valid = verify(context.token, 'supersecret');
       if (!valid) {
         throw new CustomError('Ação nao autorizada', 401);
-=======
-    createUser: async (_: string, { name, email, password, birthDate, token }) => {
-      const payload = jwt.verify(token, 'supersecret');
-      if (payload == 'verifyied') {
-        const repository = getRepository(User);
-
-        const user = new User();
-
-        user.name = name;
-        user.email = email;
-        user.birthDate = birthDate;
-
-        let validPassword = true;
-        let validEmail = true;
-
-        const sameEmail = await repository.find({ email: user.email });
-        const originalPassword = password;
-
-        if (originalPassword.length < 7) {
-          validPassword = false;
-        } else if (originalPassword.search(/[0-9]/) == -1) {
-          validPassword = false;
-        } else if (originalPassword.search(/[a-z]/) == -1 && originalPassword.search(/[A-Z]/) == -1) {
-          validPassword = false;
-        } else if (sameEmail.length !== 0) {
-          validEmail = false;
-        }
-
-        if (validPassword && validEmail) {
-          const saltRounds = 0;
-          const hashPassword = await hash(originalPassword, saltRounds);
-
-          user.password = hashPassword;
-          const response = await repository.save(user);
-
-          return response;
-        } else if (validEmail == false) {
-          throw new CustomError(
-            'Esse e-mail já está cadastrado',
-            400,
-            'you can`t have more then one user in the database with the same email',
-          );
-        } else if (validPassword == false) {
-          throw new CustomError('Senha inválida', 400, 'the password doesn`t have de minimum requirements');
-        }
-      } else {
-        throw new CustomError('Seu login expirou, faça novamente', 401, 'invalid or inexisting token');
->>>>>>> 6b1acd3... autenticacao
       }
 
       const repository = getRepository(User);
@@ -141,20 +88,10 @@ export const resolvers = {
 
     login: async (_, { data: args }: { data: LonginInput }) => {
       const repository = getRepository(User);
-<<<<<<< HEAD
       const userData = await repository.findOne({ email: args.email });
 
       if (!userData) {
         throw new CustomError('Email não cadastrado', 404);
-=======
-      const userData = await repository.findOne({ email });
-      if (userData == undefined) {
-        throw new UserInputError('Email não cadastrado');
-      } else if (compare(password, userData.password)) {
-        return userData;
-      } else {
-        throw new UserInputError('Senha incorreta');
->>>>>>> 6b1acd3... autenticacao
       }
       const match = await compare(args.password, userData.password);
 

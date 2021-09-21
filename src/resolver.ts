618a5c1927da6428e11f1c73e39d0edacfb560bf
Angeloantolini <angelo.antolini@hotmail.com>
2021-09-21 09:42:09 -0300

    faker e inicio do teste

--- src/resolver.ts
+++ src/resolver.ts
@@ -25,22 +25,33 @@ export const resolvers = {
       let pageBefore = false;
       let pageAfter = false;
       let shown = '';
+      let list = [];
+
       const repository = getRepository(User);
+
       quantity == undefined ? (quantity = 10) : quantity;
       page == undefined ? (page = 0) : (page += -1);
       const skip = page * quantity;
-      const list = await repository.createQueryBuilder().orderBy('name').limit(quantity).offset(skip).getMany();
-      if (page > 0) {
-        pageBefore = true;
-      }
-      if ((await repository.findOne({ id: skip + quantity + 1 })) != undefined) {
-        pageAfter = true;
-      }
+
       const totalUsers = await repository.count();
-      if (totalUsers > skip + quantity) {
-        shown = `users:${skip + 1}-${skip + quantity}/${totalUsers}`;
+
+      if (page < 0 || skip > totalUsers) {
+        throw new CustomError('Esta página nao existe', 404);
+      } else if (quantity == 0) {
+        throw new CustomError('Insira uma quantidade não nula de usuários por pagina', 400);
       } else {
-        shown = `users:${skip + 1}-${totalUsers}/${totalUsers}`;
+        list = await repository.createQueryBuilder().orderBy('name').limit(quantity).offset(skip).getMany();
+        if (page > 0) {
+          pageBefore = true;
+        }
+        if ((await repository.findOne({ id: skip + quantity + 1 })) != undefined) {
+          pageAfter = true;
+        }
+        if (totalUsers > skip + quantity) {
+          shown = `users:${skip + 1}-${skip + quantity}/${totalUsers}`;
+        } else {
+          shown = `users:${skip + 1}-${totalUsers}/${totalUsers}`;
+        }
       }
 
       return { list, pageBefore, pageAfter, shown };

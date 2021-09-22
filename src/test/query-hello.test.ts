8f5078882d6bfaf68f387bbc24fad4b02adf6d22
Angeloantolini <angelo.antolini@hotmail.com>
2021-09-22 15:48:15 -0300

    teste erro query

--- src/test/query-hello.test.ts
+++ src/test/query-hello.test.ts
@@ -1,7 +1,11 @@
 import { queryRequest } from './constants';
 import { expect } from 'chai';
 
+<<<<<<< HEAD
 export const queryTest = describe('Query test', function () {
+=======
+describe('Query hello test', function () {
+>>>>>>> f3dc2aa... teste erro query
   it('should query Hello', async () => {
     const query = await queryRequest(`query { hello }`, {});
 
